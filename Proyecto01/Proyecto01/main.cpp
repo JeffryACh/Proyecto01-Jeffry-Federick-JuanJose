0f65c063367b411c9986fa5e1eededdd2b52d79e
JeffryACh <jeffry2811ach@gmail.com>
2025-09-28 15:40:40 -0600

    Base

---
+++ Proyecto01/Proyecto01/main.cpp
@@ -0,0 +1,19 @@
+/*
+* @Authors:
+*   - Jeffry Araya Ch.
+*   - Federick Fernadez C.
+*   - Juan J. Rojas
+*
+* Created: 28/09/2025 15:40
+*
+* Modified:
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	cout << "Hello, World!" << endl;
+}
